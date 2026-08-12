9850de93c684aceafd0190f5a33242039062e3c8
vitthala27 <vitthalgole7@gmail.com>
2026-08-13 01:51:08 +0530

    added unit test cases

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+describe("Kubernetes Demo App", function () {
+
+  it("should pass basic application test", function () {
+    assert.strictEqual(1 + 1, 2);
+  });
+
+});
